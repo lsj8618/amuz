efeeb4e541a5c56a6e82bf891e0dcafcf95ddcc0
113-01 <a113-01@113-01ui-iMac.local>
2023-09-18 23:33:38 +0900

    first

---
+++ resources/views/create.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>게시글 작성</title>
+    <style>
+        textarea {
+            width: 25%;
+            height: 300px;
+        }
+        #title {
+            width: 500px;
+        }
+        body{
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>게시글 작성</h2>
+    <form>
+        <label>제목:</label>
+        <input id="title" type="text"><br><br>
+        <label>작성자:</label>
+        <input id="title" type="text"><br><br>
+        <Textarea></Textarea><br>
+        <Input type=submit value="게시글 등록">
+    </form>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>게시판</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 20px;
+        }
+        h1 {
+            color: black;
+            padding: 10px;
+            border-radius: 5px;
+        }
+        table {
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid #ccc;
+        }
+        th, td {
+            padding: 10px;
+        }
+        th {
+            background-color: #333;
+            color: #fff;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        #createBtn {
+            left: 38%;
+            position: relative;
+            background-color: black;
+            color: white;
+            border: 1px solid black;
+            border-radius: 15px;
+            padding: 3px 11px;
+            text-decoration:none;
+        }
+    </style>
+</head>
+<body>
+    <h1>게시판</h1>
+    <a id='createBtn' href="{{route('create')}}">글작성</a>
+    <table>
+        <tr>
+            <th>글 번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성 시간</th>
+        </tr>
+        <!-- 예시 게시물 -->
+        <tr>
+            <td>1</td>
+            <td><a href="post1.html" style="text-decoration: none; color: #0078d4;">게시물 제목 1</a></td>
+            <td>사용자1</td>
+            <td>2023-09-18 14:30:00</td>
+        </tr>
+        <!-- 더 많은 게시물 추가 -->
+    </table>
+</body>
+</html>
